9976fa74a1cda4cc433c9bb25fee8edf56c66f30
Nikolay Valasatau <mikalai.valasatau@mapbox.com>
2021-12-04 20:03:56 +0300

    Day 03

--- AdventOfCode2021/main.swift
+++ AdventOfCode2021/main.swift
@@ -94,9 +94,83 @@ func task02_2(_ input: TaskInput) {
     print("T02_2: \(x)*\(y) = \(x * y)")
 }
 
+// MARK: - Day 03
+
+extension TaskInput {
+    func task03() -> [[Bool]] {
+        readInput("03")
+            .split(separator: "\n")
+            .map { line in line.map { $0 == "1" } }
+    }
+}
+
+func task03_1(_ input: TaskInput) {
+    let lines = input.task03()
+
+    var nums = [Int](repeating: 0, count: lines.first!.count)
+    for line in lines {
+        for (idx, val) in line.enumerated() {
+            nums[idx] += val ? 1 : 0
+        }
+    }
+    let domination = lines.count / 2
+    var gamma = 0
+    var epsilon = 0
+    for val in nums {
+        if val > domination {
+            gamma = gamma * 2 + 1
+            epsilon = epsilon * 2
+        } else {
+            gamma = gamma * 2
+            epsilon = epsilon * 2 + 1
+        }
+    }
+    print("T03_1: \(gamma)*\(epsilon)=\(gamma * epsilon)")
+}
+
+func task03_2(_ input: TaskInput) {
+    let lines = input.task03()
+
+    func filter(_ values: [[Bool]], idx: Int = 0, flag: Bool) -> [[Bool]] {
+        let count = values.map({ $0[idx] ? 1 : 0 }).reduce(0, +)
+        let expected = (values.count % 2 == 0 && count == values.count / 2)
+            ? flag
+            : flag ? (count > values.count / 2) : (count <= values.count / 2)
+//        print("\(count)/\(values.count) : \(expected)")
+        return values.filter { $0[idx] == expected }
+    }
+
+    func calc(_ values: [[Bool]], flag: Bool) -> Int {
+        var result = values
+        for idx in 0..<(values.first!.count) {
+            result = filter(result, idx: idx, flag: flag)
+//            print("\(result.map { l in l.map { $0 ? "1": "0" }.joined() })")
+            if result.count == 1 {
+                var num = 0
+                for val in result.first! {
+                    num *= 2
+                    if val {
+                        num += 1
+                    }
+                }
+                return num
+            }
+        }
+        assertionFailure("Whops!")
+        return 0
+    }
+
+    let o2 = calc(lines, flag: true)
+//    print("-")
+    let co2 = calc(lines, flag: false)
+    print("T03_2: \(o2) * \(co2) = \(o2 * co2)")
+}
+
+// MARK: - Main
+
 let inputs = [
-    TaskInput(),
     TaskInput(prefix: "sample"),
+    TaskInput(),
 ]
 
 for input in inputs {
@@ -106,4 +180,7 @@ for input in inputs {
 
     task02_1(input)
     task02_2(input)
+
+    task03_1(input)
+    task03_2(input)
 }
